8ccbb9286e8e19191c96cca5c2b331889e339281
AdityaPal10 <71257358+AdityaPal10@users.noreply.github.com>
2021-11-17 21:57:11 -0500

    Delete GameLauncher.java

--- GameLauncher.java
+++
@@ -1,8 +0,0 @@
-public class GameLauncher {
-    public static void main(String[] args) {
-    	SoundFactory soundClass = new SoundFactory();
-    	soundClass.playBackGround("StarWars60");
-        //Legends m=new Legends();
-        //m.startGame();
-    }
-}
