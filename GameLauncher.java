211e781b7000d3243d5a220955192bf2fc5a27da
AdityaPal10 <71257358+AdityaPal10@users.noreply.github.com>
2021-11-17 21:55:58 -0500

    Add files via upload
    add color and interface changes

---
+++ GameLauncher.java
@@ -0,0 +1,8 @@
+public class GameLauncher {
+    public static void main(String[] args) {
+    	SoundFactory soundClass = new SoundFactory();
+    	soundClass.playBackGround("StarWars60");
+        //Legends m=new Legends();
+        //m.startGame();
+    }
+}
